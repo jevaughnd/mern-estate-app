5347321c7015efcab2ed9e6199fb1c2e8f66dbdd
jevaughndyer <93652759+jevaughnd@users.noreply.github.com>
2024-02-19 10:13:16 -0500

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import About from './Pages/About'
+import Profile from './Pages/Profile'
 
 export default function App() {
-  return (
-    <h1 className='text-blue-500'>Mern App</h1>
-  )
+  return <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Home/>}></Route>
+    <Route path='sign-in' element={<SignIn/>}></Route>
+    <Route path='sign-up' element={<SignUp/>}></Route>
+    <Route path='about' element={<About/>}></Route>
+    <Route path='profile' element={<Profile/>}></Route>
+     
+
+  </Routes>
+  
+  </BrowserRouter>
+
+
+  
 }
 
