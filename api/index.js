cad71afcca08cb5fec51c2afdd80382103403656
jevaughndyer <93652759+jevaughnd@users.noreply.github.com>
2024-02-23 11:04:26 -0500

    connection string mod

--- api/index.js
+++ api/index.js
@@ -2,21 +2,24 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
-
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
 mongoose
     .connect(process.env.MONGO)
-    .then(()=>{
-        console.log('Connected to MongoDB!');
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+    .then(() => {
+    console.log('Connected to MongoDB!');
+})
+.catch((err) => {
+    console.log(err);
+});
 
 
 const app = express();
+
+//send json to server
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
@@ -24,3 +27,5 @@ app.listen(3000, () => {
 );
 
 app.use("/api/user", userRouter)
+app.use("/api/auth", authRouter)
+
