c1e01549c6ef4be503fb5a42544c259df4fc3b3b
jevaughndyer <93652759+jevaughnd@users.noreply.github.com>
2024-02-26 16:18:43 -0500

    create sign in api route

--- api/Controllers/auth.controller.js
+++ api/Controllers/auth.controller.js
@@ -1,7 +1,12 @@
 import User from "../Models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
+
+
+
+//Sign Up
 export const signup = async (req, res, next) =>{
 
     const {username, email, password} = req.body;
@@ -17,3 +22,31 @@ export const signup = async (req, res, next) =>{
     }
 };
 
+//Sign In
+//first check email, if email does not exist, return error, otherwise check passord.
+export const signin = async (req, res, next) =>{
+    const {email, password} = req.body;
+
+    try{
+        const validUser = await User.findOne({email});
+        if (!validUser) return next (errorHandler(404, "User Not found"));
+
+        //compare saved hashed password in database with bcryptjs method compareSync
+        const validPassword = bcryptjs.compareSync(password, validUser.password);     
+    
+        if (!validPassword) return next (errorHandler(401, 'Wrong Credentials Used!'));
+
+        // token is equal to the unique user id
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+        //remove password before sending to user in front end, seperate password and rest of information
+        const {password: pass, ...rest} = validUser._doc;
+
+        //save token in cookie || instead of returing validUser return rest so it dosent show the user password in the front end
+        res.cookie('access_token', token, {httpOnly: true}).status(200).json(rest); 
+
+    
+    }catch (error){
+        next(error);
+    }
+}
--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -74,6 +74,7 @@ export default function SignUp() {
             <span className='text-blue-700'>Sign In</span>
           </Link>
         </div>
+        
         {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   )
